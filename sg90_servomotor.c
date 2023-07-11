a82ad21c3b01bf31476a5995d88fc0bec1fcb9c6
Thrasherop <ultraprim@gmail.com>
2023-07-11 12:43:35 -0600

    Finished the code

--- sg90_servomotor.c
+++ sg90_servomotor.c
@@ -49,6 +49,20 @@ void init_fingers();
 void move_joint(int finger, int joint, int percent);
 void cycle_finger(int finger);
 
+// High level movement functions
+void close_hand();
+void open_hand();
+void c();
+void l();
+void f();
+void x();
+void s();
+void b();
+void i();
+void w();
+void y();
+void o();
+
 // Declare 2D array
 PWM_Handle fingers[6][2];
 
@@ -63,29 +77,209 @@ void *mainThread(void *arg0)
 
     init_fingers();
 
+
+
+//    move_joint(4, 0, 90);
+//    move_joint(2, 0, 90);
+
+//    open_hand();
+
+    int letter_gap = 2;
+    int word_gap = 4;
+
+
+
     while(1){
 
-//        move_joint(0, 0, 25);
+//        close_hand();
 //        sleep(1);
-//        move_joint(0, 0, 99);
+//        open_hand();
 //        sleep(1);
 
-//        cycle_finger(2);
+//        c();
+//        sleep(3);
+//        open_hand();
+//        sleep(2);
 
-        move_joint(5, 0, 25);
-        sleep(2);
-        move_joint(5, 0, 90);
-        sleep(2);
-//        move_joint(2, 0, 25);
-//        sleep(2);
-//        move_joint(3, 0, 25);
-//        sleep(2);
-//        move_joint(4, 0, 25);
-//        sleep(2);
-//        move_joint(5, 0, 25);
-//        sleep(2);
+        // Billy
+        b();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        i();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        l();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        y();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        s();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+
+        // cow
+        sleep(word_gap); // sentence
+
+        c();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        o();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        w();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+
+        // is
+        sleep(word_gap); // word space
+
+
+        i();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        s();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+
+        // slow
+        sleep(word_gap); // space
+
+        s();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        l();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        o();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+        w();
+        sleep(letter_gap);
+        open_hand();
+        sleep(letter_gap);
+
+
 
     }
+
+}
+
+void o(){
+   // set fingers one by one
+   move_joint(5, 0, 30);
+   sleep(1);
+   move_joint(1, 0, 25);
+   move_joint(2, 0, 30);
+   move_joint(4, 0, 30);
+}
+
+void y(){
+    move_joint(2, 0, 25);
+    move_joint(3, 0, 25);
+}
+
+void w(){
+    move_joint(5, 0, 25);
+    move_joint(1, 0, 25);
+}
+
+void i(){
+
+    move_joint(2, 0, 25);
+    move_joint(3, 0, 25);
+    move_joint(1, 0, 25);
+
+}
+
+void b(){
+
+    move_joint(1, 0, 25);
+
+}
+
+void s() {
+
+    move_joint(5, 0, 25);
+    sleep(1);
+    move_joint(3, 0, 25);
+    move_joint(2, 0, 25);
+    sleep(1);
+    move_joint(1, 0, 25);
+
+}
+
+void x(){
+
+    move_joint(5, 0, 25);
+    sleep(1);
+    move_joint(3, 0, 25);
+    move_joint(2, 0, 60);
+    sleep(1);
+    move_joint(1, 0, 25);
+
+}
+
+void f(){
+
+    move_joint(1, 0, 25);
+    move_joint(2, 0, 25);
+
+}
+
+void l(){
+
+    move_joint(5,0,25);
+    sleep(1);
+    move_joint(4, 0, 25);
+    sleep(1);
+
+}
+
+void c(){
+
+    // set fingers one by one
+    move_joint(5, 0, 50);
+    sleep(1);
+    move_joint(1, 0, 35);
+    move_joint(2, 0, 40);
+    move_joint(4, 0, 30);
+
+}
+
+void close_hand(){
+
+    // Close fingers one by one
+    move_joint(5, 0, 25);
+    sleep(1);
+    move_joint(1, 0, 25);
+    move_joint(2, 0, 25);
+    move_joint(4, 0, 25);
+
+}
+
+void open_hand(){
+
+    // Close fingers one by one
+    move_joint(1, 0, 90);
+    move_joint(2, 0, 90);
+    move_joint(4, 0, 90);
+    sleep(1);
+    move_joint(5, 0, 90);
+
 
 }
 
@@ -115,6 +309,9 @@ void init_fingers(){
     fingers[3][0] = finger34Handle;
     fingers[4][0] = finger34Handle;
     fingers[5][0] = finger5Handle;
+
+    // Open all the fingers
+    open_hand();
 
 }
 
